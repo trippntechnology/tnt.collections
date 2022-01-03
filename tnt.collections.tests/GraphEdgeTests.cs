95d250377f2d972fca1d875587bcded93b8eb627
Nate Tripp <nate.tripp13448@gmail.com>
2022-01-03 10:02:46 -0700

    Add project files.

---
+++ tnt.collections.tests/GraphEdgeTests.cs
@@ -0,0 +1,175 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TNT.Collections;
+
+namespace Tests
+{
+   [TestClass]
+   public class GraphEdgeTests
+   {
+      [TestMethod]
+      public void OperatorEqualTest()
+      {
+         object eo1 = new object();
+         object eo2 = new object();
+
+         object no1 = new object();
+         object no2 = new object();
+         object no3 = new object();
+
+         GraphNode<object, object> gn1 = new GraphNode<object, object>(no1);
+         GraphNode<object, object> gn2 = new GraphNode<object, object>(no2);
+         GraphNode<object, object> gn3 = new GraphNode<object, object>(no3);
+
+
+         GraphEdge<object, object> ge1 = new GraphEdge<object, object>(eo1, gn1, gn2);
+         GraphEdge<object, object> ge2 = new GraphEdge<object, object>(eo1, gn1, gn2);
+         GraphEdge<object, object> ge3 = new GraphEdge<object, object>(eo2, gn2, gn3);
+
+         Assert.IsFalse(ge1 == null);
+         Assert.IsTrue(ge1 == ge1);
+         Assert.IsTrue(ge1 == ge2);
+         Assert.IsFalse(ge1 == ge3);
+
+         Assert.IsTrue(ge1 == eo1);
+         Assert.IsFalse(ge1 == eo2);
+
+         Assert.IsTrue(eo1 == ge1);
+         Assert.IsFalse(eo2 == ge2);
+
+         Assert.IsTrue(ge1 != null);
+         Assert.IsFalse(ge1 != ge1);
+         Assert.IsFalse(ge1 != ge2);
+         Assert.IsTrue(ge1 != ge3);
+
+         Assert.IsFalse(ge1 != eo1);
+         Assert.IsTrue(ge1 != eo2);
+
+         Assert.IsFalse(eo1 != ge1);
+         Assert.IsTrue(eo2 != ge2);
+      }
+
+      [TestMethod]
+      public void EqualsTest()
+      {
+         object eo1 = new object();
+         object eo2 = new object();
+
+         object no1 = new object();
+         object no2 = new object();
+         object no3 = new object();
+
+         GraphNode<object, object> gn1 = new GraphNode<object, object>(no1);
+         GraphNode<object, object> gn2 = new GraphNode<object, object>(no2);
+         GraphNode<object, object> gn3 = new GraphNode<object, object>(no3);
+
+
+         GraphEdge<object, object> ge1 = new GraphEdge<object, object>(eo1, gn1, gn2);
+         GraphEdge<object, object> ge2 = new GraphEdge<object, object>(eo1, gn1, gn2);
+         GraphEdge<object, object> ge3 = new GraphEdge<object, object>(eo2, gn2, gn3);
+
+         Assert.IsTrue(ge1.Equals(ge1));
+         Assert.IsTrue(ge1.Equals(ge2));
+         Assert.IsFalse(ge1.Equals(eo1));
+         Assert.IsFalse(ge1.Equals(null));
+         Assert.IsFalse(ge1.Equals(ge3));
+      }
+
+      [TestMethod]
+      public void GetHashCodeTest()
+      {
+         object eo1 = new object();
+         object no1 = new object();
+         object no2 = new object();
+
+         GraphNode<object, object> gn1 = new GraphNode<object, object>(no1);
+         GraphNode<object, object> gn2 = new GraphNode<object, object>(no2);
+
+         GraphEdge<object, object> ge1 = new GraphEdge<object, object>(eo1, gn1, gn2);
+
+         Assert.AreEqual(eo1.GetHashCode(), ge1.GetHashCode());
+      }
+
+      [TestMethod]
+      public void ConstructorTests()
+      {
+         object eo1 = new object();
+         object no1 = new object();
+         object no2 = new object();
+
+         GraphNode<object, object> gn1 = new GraphNode<object, object>(no1);
+         GraphNode<object, object> gn2 = new GraphNode<object, object>(no2);
+
+
+         Assert.ThrowsException<ArgumentNullException>(() => new GraphEdge<object, object>(null, gn1, gn2));
+      }
+
+      [TestMethod]
+      public void ConstructorTests1()
+      {
+         object eo1 = new object();
+         object no1 = new object();
+         object no2 = new object();
+
+         GraphNode<object, object> gn1 = new GraphNode<object, object>(no1);
+         GraphNode<object, object> gn2 = new GraphNode<object, object>(no2);
+
+         Assert.ThrowsException<ArgumentNullException>(() => new GraphEdge<object, object>(eo1, null, gn2));
+
+      }
+
+      [TestMethod]
+      public void ConstructorTests2()
+      {
+         object eo1 = new object();
+         object no1 = new object();
+         object no2 = new object();
+
+         GraphNode<object, object> gn1 = new GraphNode<object, object>(no1);
+         GraphNode<object, object> gn2 = new GraphNode<object, object>(no2);
+
+         Assert.ThrowsException<ArgumentNullException>(() => new GraphEdge<object, object>(eo1, gn1, null));
+
+      }
+
+      [TestMethod]
+      [ExpectedException(typeof(ArgumentException))]
+      public void ConstructorTests3()
+      {
+         object eo1 = new object();
+         object no1 = new object();
+         object no2 = new object();
+
+         GraphNode<object, object> gn1 = new GraphNode<object, object>(no1);
+         GraphNode<object, object> gn2 = new GraphNode<object, object>(no2);
+
+         try
+         {
+            new GraphEdge<object, object>(eo1, gn1, gn1);
+         }
+         catch (ArgumentException ae)
+         {
+            Assert.AreEqual("Arguments node1 and node2 can not be the same node", ae.Message);
+            throw;
+         }
+      }
+
+      [TestMethod]
+      public void GetOppositeNodeTest()
+      {
+         object eo1 = new object();
+         object no1 = new object();
+         object no2 = new object();
+         object no3 = new object();
+
+         GraphNode<object, object> gn1 = new GraphNode<object, object>(no1);
+         GraphNode<object, object> gn2 = new GraphNode<object, object>(no2);
+         GraphNode<object, object> gn3 = new GraphNode<object, object>(no3);
+
+         GraphEdge<object, object> ge1 = new GraphEdge<object, object>(eo1, gn1, gn2);
+
+         Assert.ThrowsException<ArgumentNullException>(() => ge1.GetOppositeNode(null));
+
+      }
+   }
+}
---
+++ tnt.collections.tests/GraphTests.cs
@@ -0,0 +1,178 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TNT.Collections;
+
+namespace Tests
+{
+	[TestClass]
+	public class GraphTests
+	{
+		[TestMethod]
+		public void ConstructorTests()
+		{
+			Graph<object, object> g = new Graph<object, object>();
+			Assert.IsFalse(g.AllowCircularLinks);
+
+			g = new Graph<object, object>(true);
+			Assert.IsTrue(g.AllowCircularLinks);
+		}
+
+		[TestMethod]
+		public void CreateEdgeTestValidParameters()
+		{
+			object eo1 = new object();
+			object eo2 = new object();
+			object no1 = new object();
+			object no2 = new object();
+
+			Graph<object, object> g = new Graph<object, object>();
+
+			Assert.ThrowsException<ArgumentNullException>(() => g.CreateEdge(null, no1, no2));
+		}
+
+		[TestMethod]
+		public void CreateEdgeTestValidParameters1()
+		{
+			object eo1 = new object();
+			object eo2 = new object();
+			object no1 = new object();
+			object no2 = new object();
+
+			Graph<object, object> g = new Graph<object, object>();
+			Assert.ThrowsException<ArgumentNullException>(() => g.CreateEdge(eo1, null, no2));
+		}
+
+		[TestMethod]
+		public void CreateEdgeTestValidParameters2()
+		{
+			object eo1 = new object();
+			object eo2 = new object();
+			object no1 = new object();
+			object no2 = new object();
+
+			Graph<object, object> g = new Graph<object, object>();
+
+			Assert.ThrowsException<ArgumentNullException>(() => g.CreateEdge(eo1, no1, null));
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentException))]
+		public void CreateEdgeTestValidParameters3()
+		{
+			object eo1 = new object();
+			object eo2 = new object();
+			object no1 = new object();
+			object no2 = new object();
+
+			Graph<object, object> g = new Graph<object, object>();
+
+			try
+			{
+				g.CreateEdge(eo1, no1, no1);
+			}
+			catch (ArgumentException ae)
+			{
+				Assert.AreEqual("Arguments nodeObj1 and nodeObj2 can not be the same object", ae.Message);
+				throw;
+			}
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentException))]
+		public void CreateEdgeTestExistingEdgeNode()
+		{
+			object eo1 = new object();
+			object eo2 = new object();
+			object no1 = new object();
+			object no2 = new object();
+
+			Graph<object, object> g = new Graph<object, object>(true);
+
+			GraphEdge<object, object> edge = g.CreateEdge(eo1, no1, no2);
+
+			Assert.AreEqual(eo1, edge.Object);
+
+			try
+			{
+				g.CreateEdge(eo1, no1, no2);
+			}
+			catch (ArgumentException ae)
+			{
+				Assert.AreEqual("edgeObj is already bound to an edge", ae.Message);
+				throw;
+			}
+		}
+		
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentException))]
+		public void CreateEdgeTestCircularLink()
+		{
+			object eo1 = new object();
+			object eo2 = new object();
+			object eo3 = new object();
+			object no1 = new object();
+			object no2 = new object();
+			object no3 = new object();
+
+			Graph<object, object> g = new Graph<object, object>();
+
+			g.CreateEdge(eo1, no1, no2);
+			g.CreateEdge(eo2, no2, no3);
+
+			try
+			{
+				g.CreateEdge(eo3, no3, no1);
+			}
+			catch (ArgumentException ae)
+			{
+				Assert.AreEqual("Circular link not allowed", ae.Message);
+				throw;
+			}
+		}
+
+
+		[TestMethod]
+		public void FindGraphNodeTest()
+		{
+			Graph<object, object> g = new Graph<object, object>();
+			GraphNode<object, object> gn = g.FindGraphNode(new object());
+			Assert.IsNull(gn);
+
+			gn = g.FindGraphNode(null);
+			Assert.IsNull(gn);
+		}
+
+		[TestMethod]
+		public void FindEdgeNodeTest()
+		{
+			Graph<object, object> g = new Graph<object, object>();
+			GraphEdge<object, object> ge = g.FindGraphEdge(new object());
+			Assert.IsNull(ge);
+
+			ge = g.FindGraphEdge(null);
+			Assert.IsNull(ge);
+		}
+
+		[TestMethod]
+		public void FindConnectedNodeTest()
+		{
+			object eo1 = new object();
+			object eo2 = new object();
+			object eo3 = new object();
+			object eo4 = new object();
+			object no1 = new object();
+			object no2 = new object();
+			object no3 = new object();
+			object no4 = new object();
+
+			Graph<object, object> g = new Graph<object, object>();
+
+			GraphEdge<object, object> e1 = g.CreateEdge(eo1, no1, no2);
+			GraphEdge<object, object> e2 = g.CreateEdge(eo2, no2, no3);
+			GraphEdge<object, object> e3 = g.CreateEdge(eo3, no2, no4);
+
+			Assert.ThrowsException<ArgumentNullException>(() => g.FindConnectedNode(null, null, true));
+		}
+	}
+}
